d0bacd89817a6f8ec974bd0344d384539cd24138
Kalena Almand <kalmand1057@gmail.com>
2020-10-07 04:52:42 -0500

    added api get and post requests to apiroutes

---
+++ Routes/apiRoutes.js
@@ -0,0 +1,18 @@
+//LOAD DATA
+var db = require("../Develop/db");
+
+//ROUTING
+module.exports = function(app) {
+    app.get("/api/notes", function(req, res) {
+        res.json(db);
+    });
+
+    app.post("/api/notes", function(req, res) {
+        db.push(req.body);
+        res.json(req.body);
+    });
+
+    // app.delete("/api/notes/:id", function(req, res) {
+    //     //figure out id for notes
+    // });
+};
